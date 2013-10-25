dc81a916c42240ce6123ca68bdc4c95d973afb4c
asarium <asarium@gmail.com>
2013-10-25 11:11:15 +0200

    Formatting and comment cleanup

--- cmake/gcc.cmake
+++ cmake/gcc.cmake
@@ -3,5 +3,5 @@ MESSAGE(STATUS "Doing configuration specific to gcc...")
 
 INCLUDE(util)
 
-# Set /EHsc
 ADD_COMPILER_FLAG("-Wno-unused-parameter -Wno-write-strings" "-Wno-unused-parameter -Wno-write-strings")
+
