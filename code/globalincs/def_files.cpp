dbf4c8cb4fcf7a4ccd03778d20fbfd1fa6d802e9
asarium <asarium@gmail.com>
2013-11-04 22:54:55 +0100

    Move text embedder into embedfile

--- code/globalincs/def_files.cpp
+++ code/globalincs/def_files.cpp
@@ -20,81 +20,7 @@ typedef struct def_file
 
 //:PART 1:
 //**********
-extern char* Default_ai_profiles_tbl;
-extern char* Default_autopilot_tbl;
-extern char* Default_blur_f_sdr;
-extern char* Default_brightpass_f_sdr;
-extern char* Default_controlconfigdefaults_tbl;
-extern char* Default_fonts_tbl;
-extern char* Default_fxaa_f_sdr;
-extern char* Default_fxaa_v_sdr;
-extern char* Default_fxaapre_f_sdr;
-extern char* Default_game_settings_tbl;
-extern char* Default_iff_defs_tbl;
-extern char* Default_ls_f_sdr;
-extern char* Default_main_f_sdr;
-extern char* Default_main_v_sdr;
-extern char* Default_objecttypes_tbl;
-extern char* Default_particle_f_sdr;
-extern char* Default_particle_v_sdr;
-extern char* Default_post_f_sdr;
-extern char* Default_post_v_sdr;
-extern char* Default_post_processing_tbl;
-extern char* Default_species_defs_tbl;
-extern char* Default_video_f_sdr;
-extern char* Default_video_v_sdr;
 //**********
-
-//:PART 2:
-//**********
-def_file Default_files[] =
-{
-	{ "ai_profiles.tbl",		Default_ai_profiles_tbl},
-	{ "autopilot.tbl",			Default_autopilot_tbl},
-	{ "blur-f.sdr",				Default_blur_f_sdr},
-	{ "brightpass-f.sdr",		Default_brightpass_f_sdr},
-	{ "controlconfigdefaults.tbl",	Default_controlconfigdefaults_tbl},
-	{ "fonts.tbl",				Default_fonts_tbl},
-	{ "fxaa-f.sdr",				Default_fxaa_f_sdr},
-	{ "fxaa-v.sdr",				Default_fxaa_v_sdr},
-	{ "fxaapre-f.sdr",			Default_fxaapre_f_sdr},
-	{ "game_settings.tbl",		Default_game_settings_tbl},
-	{ "iff_defs.tbl",			Default_iff_defs_tbl},
-	{ "ls-f.sdr",				Default_ls_f_sdr},
-	{ "main-f.sdr",				Default_main_f_sdr},
-	{ "main-v.sdr",				Default_main_v_sdr},
-	{ "objecttypes.tbl",		Default_objecttypes_tbl},
-	{ "particle-f.sdr",			Default_particle_f_sdr},
-	{ "particle-v.sdr",			Default_particle_v_sdr},
-	{ "post-f.sdr",				Default_post_f_sdr},
-	{ "post-v.sdr",				Default_post_v_sdr},
-	{ "post_processing.tbl",	Default_post_processing_tbl},
-	{ "species_defs.tbl",		Default_species_defs_tbl},
-	{ "video-f.sdr",			Default_video_f_sdr},
-	{ "video-v.sdr",			Default_video_v_sdr},
-};
-
-static int Num_default_files = sizeof(Default_files) / sizeof(def_file);
-//**********
-
-char *defaults_get_file(char *filename)
-{
-	for(int i = 0; i < Num_default_files; i++)
-	{
-		if(!stricmp(Default_files[i].filename, filename))
-		{
-			return Default_files[i].contents;
-		}
-	}
-
-	//WMC - This is really bad, because it means we have a default table missing.
-	Error(LOCATION, "Default table '%s' missing from executable - contact a coder.", filename);
-	return NULL;
-}
-
-//:PART 3:
-//**********
-//=========================================================================
 
 // These files are generated from the actual files, if you add a new file add the #include below
 
@@ -143,4 +69,54 @@ char *defaults_get_file(char *filename)
 #include "def_files/video-f.sdr.h"
 
 #include "def_files/video-v.sdr.h"
+//:PART 2:
+//**********
+def_file Default_files[] =
+{
+	{ "ai_profiles.tbl",		(char*) Default_ai_profiles_tbl},
+	{ "autopilot.tbl",			(char*) Default_autopilot_tbl},
+	{ "blur-f.sdr",				(char*) Default_blur_f_sdr},
+	{ "brightpass-f.sdr",		(char*) Default_brightpass_f_sdr},
+	{ "controlconfigdefaults.tbl",	(char*) Default_controlconfigdefaults_tbl},
+	{ "fonts.tbl",				(char*) Default_fonts_tbl},
+	{ "fxaa-f.sdr",				(char*) Default_fxaa_f_sdr},
+	{ "fxaa-v.sdr",				(char*) Default_fxaa_v_sdr},
+	{ "fxaapre-f.sdr",			(char*) Default_fxaapre_f_sdr},
+	{ "game_settings.tbl",		(char*) Default_game_settings_tbl},
+	{ "iff_defs.tbl",			(char*) Default_iff_defs_tbl},
+	{ "ls-f.sdr",				(char*) Default_ls_f_sdr},
+	{ "main-f.sdr",				(char*) Default_main_f_sdr},
+	{ "main-v.sdr",				(char*) Default_main_v_sdr},
+	{ "objecttypes.tbl",		(char*) Default_objecttypes_tbl},
+	{ "particle-f.sdr",			(char*) Default_particle_f_sdr},
+	{ "particle-v.sdr",			(char*) Default_particle_v_sdr},
+	{ "post-f.sdr",				(char*) Default_post_f_sdr},
+	{ "post-v.sdr",				(char*) Default_post_v_sdr},
+	{ "post_processing.tbl",	(char*) Default_post_processing_tbl},
+	{ "species_defs.tbl",		(char*) Default_species_defs_tbl},
+	{ "video-f.sdr",			(char*) Default_video_f_sdr},
+	{ "video-v.sdr",			(char*) Default_video_v_sdr},
+};
+
+static int Num_default_files = sizeof(Default_files) / sizeof(def_file);
+//**********
+
+char *defaults_get_file(char *filename)
+{
+	for(int i = 0; i < Num_default_files; i++)
+	{
+		if(!stricmp(Default_files[i].filename, filename))
+		{
+			return Default_files[i].contents;
+		}
+	}
+
+	//WMC - This is really bad, because it means we have a default table missing.
+	Error(LOCATION, "Default table '%s' missing from executable - contact a coder.", filename);
+	return NULL;
+}
+
+//:PART 3:
+//**********
+//=========================================================================
 
--- tools/embedfile/embedfile.cpp
+++ tools/embedfile/embedfile.cpp
@@ -7,6 +7,11 @@
 // Correct the code so that the program will properly output text to a target .h file
 // Correct the code so that the program will properly read bytes in and output as hexidecimal string values.
 
+#if defined _MSC_VER
+	// Disable warning for stricmp
+	#pragma warning(disable: 4996)
+#endif
+
 #include <fstream>
 #include <iostream>
 #include <string>
@@ -29,20 +34,171 @@ static const size_t INPUT_BUFFER_SIZE = 1024;
 
 typedef unsigned char ubyte;
 
+void write_byte(std::ostream& stream, ubyte byte, int i)
+{
+	stream << "0x" << std::hex << std::setw(2) << std::setfill('0') << std::uppercase << int(byte);
+
+	if( (i % 16) == 0 )
+	{
+		// End of 16-byte row
+		stream << "," << std::endl;
+		stream << "\t";
+	}
+	else
+	{
+		// Somewhere within a column
+		stream << ", ";
+	}
+}
+
+void do_binary_content(std::ifstream& file_in, std::ofstream& file_out,
+					   const std::string& field_name, size_t input_size, bool wxWidgets_image)
+{
+	if (wxWidgets_image)
+	{
+		file_out << "#include <wx/mstream.h>" << std::endl;
+		file_out << "#include <wx/image.h>" << std::endl;
+		file_out << "#include <wx/bitmap.h>" << std::endl;
+		file_out << std::endl;
+	}
+	
+	file_out << "static const unsigned char " << field_name << "[] = " << std::endl;
+	file_out << "{" << std::endl;
+	file_out << "\t";
+
+	ubyte buffer[INPUT_BUFFER_SIZE];
+
+	size_t i = 0;
+	while(i < input_size)
+	{
+		size_t current_size = std::min(input_size - i, INPUT_BUFFER_SIZE);
+		file_in.read((char*) buffer, current_size);
+
+		for (size_t j = 0; j < current_size; j++)
+		{
+			i++;
+			write_byte(file_out, buffer[j], i);
+		}
+	}
+
+	file_out << std::endl;
+
+	file_out << "};" << std::endl;
+
+	if (wxWidgets_image)
+	{
+		file_out << std::endl;
+
+		file_out << "wxBitmap& " << field_name << "_to_wx_bitmap()" << std::endl;
+		file_out << "{" << std::endl;
+		file_out << "\tstatic wxMemoryInputStream memIStream( " << field_name << ", sizeof( " << field_name << " ) );" << std::endl;
+		file_out << "\tstatic wxImage image( memIStream );" << std::endl;
+		file_out << "\tstatic wxBitmap bmp( image );" << std::endl;
+		file_out << "\treturn bmp;" << std::endl;
+		file_out << "};" << std::endl;
+	}
+}
+
+void do_text_content(std::ifstream& file_in, std::ofstream& file_out,
+					   const std::string& field_name, size_t input_size)
+{
+	const char* escapeCharacters = "\\\"\n\r";
+
+	std::string file_content;
+	file_content.reserve(input_size);
+
+	file_content.assign((std::istreambuf_iterator<char>(file_in)),
+				std::istreambuf_iterator<char>());
+	
+	file_out << "char *" << field_name << " = " << std::endl;
+	file_out << "\"";
+	
+	size_t pos = 0;
+	const char* str = file_content.c_str();
+
+	while (pos < file_content.length())
+	{
+		const char* found_ptr = strpbrk(str + pos, escapeCharacters);
+
+		if (!found_ptr)
+		{
+			file_out.write(str + pos, file_content.length() - pos);
+			break;
+		}
+		else
+		{
+			size_t found_pos = (size_t) (found_ptr - str);
+
+			size_t delta = found_pos - pos;
+
+			if (delta != 0)
+			{
+				file_out.write(str + pos, delta);
+			}
+
+			switch(str[found_pos])
+			{
+			case '\\':
+				file_out << "\\\\";
+				break;
+			case '\n':
+				file_out << "\\n\"" << std::endl << "\"";
+				break;
+			case '"':
+				file_out << "\\\"";
+				break;
+			case '\r':
+				// Discard this character, possibly breaks on mac but whatever...
+				break;
+			default:
+				std::cerr << "Invalid character encountered!" << std::endl;
+			}
+
+			pos = found_pos + 1;
+		}
+	}
+
+	file_out << "\";" << std::endl;
+}
+
 int main( int argc, char* argv[] )
 {
-	if (argc != 4)
+	if (argc < 4 || argc > 6)
 	{
-		std::cerr << "Usage: embedpng <inout> <output> <fieldname>" << std::endl;
+		std::cerr << "Usage: embedfile [-wx] [-text] <inout> <output> <fieldname>" << std::endl;
 
 		return error_invalidargs;
 	}
 
-	std::string input_file(argv[1]);
-	std::string output_file(argv[2]);
-	std::string field_name(argv[3]);
+	bool wxWidgets_image = false;
+	bool text_content = false;
 
-	// Create the file_in stream, and verify integrety
+	int argc_offset = 1;
+
+	if (!stricmp(argv[argc_offset], "-wx"))
+	{
+		wxWidgets_image = true;
+
+		argc_offset++;
+	}
+
+	if (!stricmp(argv[argc_offset], "-text"))
+	{
+		text_content = true;
+
+		argc_offset++;
+	}
+
+	std::string input_file(argv[argc_offset]);
+	std::string output_file(argv[argc_offset + 1]);
+	std::string field_name(argv[argc_offset + 2]);
+
+	std::ios::openmode mode = std::ios::in;
+	if (!text_content)
+	{
+		mode |= std::ios::binary;
+	}
+
 	std::ifstream file_in(input_file.c_str(), std::ios::binary);
 	if( file_in.bad() )
 	{
@@ -69,81 +225,21 @@ int main( int argc, char* argv[] )
 
 	// Before conditioning, use this a the name of the .h
 	std::ofstream file_out(output_file.c_str());
-	if( file_out.bad() )
+	if(file_out.bad())
 	{
 		std::cerr << "Error opening file: " << output_file << std::endl;
 		file_in.close();
 		return error_cantoutputfile;
 	}
 
-	// Condition header_name to proper format ( _HEADER_H )
-	std::string header_name;
-	header_name.reserve(field_name.length() + 3); // Reserve three additional as we need those later
-	header_name.assign(field_name);
-
-	std::transform(header_name.begin(), header_name.end(), header_name.begin(), ::toupper); // HEADER (I hate C++)
-
-	header_name.insert(0, 1, '_');	// _HEADER
-	header_name.append("_H");	// _HEADER_H
-
-	file_out << "#ifndef " << header_name << std::endl;
-	file_out << "#define " << header_name << std::endl;
-	file_out << std::endl;
-
-	file_out << "#include <wx/mstream.h>" << std::endl;
-	file_out << "#include <wx/image.h>" << std::endl;
-	file_out << "#include <wx/bitmap.h>" << std::endl;
-	file_out << std::endl;
-
-	file_out << "static const unsigned char " << field_name << "[] = " << std::endl;
-	file_out << "{" << std::endl;
-	file_out << "\t";
-
-	ubyte buffer[INPUT_BUFFER_SIZE];
-
-	size_t i = 0;
-	while(i < input_size)
+	if (text_content)
 	{
-		size_t current_size = std::min(input_size - i, INPUT_BUFFER_SIZE);
-		file_in.read((char*) buffer, current_size);
-
-		for (size_t j = 0; j < current_size; j++)
-		{
-			file_out << "0x" << std::hex << std::setw(2) << std::setfill('0') << std::uppercase << int(buffer[j]);
-			i++;
-
-			if( (i % 16) == 0 )
-			{
-				// End of 16-byte row
-				file_out << "," << std::endl;
-				file_out << "\t";
-			}
-			else
-			{
-				// Somewhere within a column
-				file_out << ", ";
-			}
-		}
+		do_text_content(file_in, file_out, field_name, input_size);
 	}
-
-	file_out << std::endl;
-
-	file_out << "};" << std::endl;
-	file_out << std::endl;
-
-	file_out << "wxBitmap& " << field_name << "_to_wx_bitmap()" << std::endl;
-	file_out << "{" << std::endl;
-	file_out << "\tstatic wxMemoryInputStream memIStream( " << field_name << ", sizeof( " << field_name << " ) );" << std::endl;
-	file_out << "\tstatic wxImage image( memIStream, wxBITMAP_TYPE_PNG );" << std::endl;
-	file_out << "\tstatic wxBitmap bmp( image );" << std::endl;
-	file_out << "\treturn bmp;" << std::endl;
-	file_out << "};" << std::endl;
-
-	file_out << std::endl;
-
-	file_out << std::endl;
-
-	file_out << "#endif // " << header_name << std::endl;
+	else
+	{
+		do_binary_content(file_in, file_out, field_name, input_size, wxWidgets_image);
+	}
 
 	file_in.close();
 	file_out.close();
