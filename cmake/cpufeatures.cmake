e5a6e1f62ef097894b8407e4adebe79ba5b1d905
asarium <asarium@gmail.com>
2013-12-01 21:44:03 +0100

    Wohoo! Automatic detection of MMX!

---
+++ cmake/cpufeatures.cmake
@@ -0,0 +1,8 @@
+
+IF(NOT CMAKE_CROSSCOMPILING) # The Cmake documentation says we should do this
+	TRY_RUN(RUN_RESULT COMPILE_RESULT "${CMAKE_BINARY_DIR}/temp" "${CMAKE_SOURCE_DIR}/cmake/cpufeatures.cpp"
+		RUN_OUTPUT_VARIABLE FEATURE_OUTPUT)
+	MESSAGE(STATUS "Detected compatibility for the ${FEATURE_OUTPUT} feature set. Optimizations are done accordingly.")
+
+	SET(FSO_INSTRUCTION_SET ${FEATURE_OUTPUT} CACHE STRING "The instruction set enhancement to use, possible options are SSE, SSE2 and AVX")
+ENDIF(NOT CMAKE_CROSSCOMPILING)
