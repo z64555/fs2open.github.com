e3a29704a665fc96cf661b9d6268fe789fba5c4f
asarium <asarium@gmail.com>
2013-12-20 15:33:40 +0100

    Some more stlport changes after feedback from Goober

--- lib/stlport.cmake
+++ lib/stlport.cmake
@@ -3,13 +3,11 @@ SET(USE_STLPORT ON)
 
 INCLUDE(ExternalProject)
 
-IF(${CMAKE_SYSTEM_VERSION} VERSION_LESS 4.10)
-	SET(STL_PLATFORM "win95")
-ELSEIF(${CMAKE_SYSTEM_VERSION} VERSION_LESS 5.1)
-	SET(STL_PLATFORM "win98")
-ELSE(${CMAKE_SYSTEM_VERSION} VERSION_LESS 4.10)
-	SET(STL_PLATFORM "winxp")
-ENDIF(${CMAKE_SYSTEM_VERSION} VERSION_LESS 4.10)
+IF(MSVC60)
+	SET(args msvc6 -p win98)
+ELSE(MSVC60)
+	SET(args msvc8)
+ENDIF(MSVC60)
 
 IF(MSVC_USE_RUNTIME_DLL)
 	SET(STL_RUNTIME_LIBRARY "--with-dynamic-rtl")
@@ -23,9 +21,9 @@ ExternalProject_Add(stlport
 	URL http://sourceforge.net/projects/stlport/files/STLport/STLport-5.2.1/STLport-5.2.1.tar.gz # Full path or URL of source
 	URL_MD5 b20ace9f4a487b2614dfbb205bee112c               # MD5 checksum of file at URL
 	# Configuring, we need to use cmd as configure calls exit
-	CONFIGURE_COMMAND cmd /C configure msvc6 -p ${STL_PLATFORM} ${STL_RUNTIME_LIBRARY}
+	CONFIGURE_COMMAND cmd /C configure ${args} ${STL_RUNTIME_LIBRARY}
 	# Building
-	BUILD_COMMAND cd build/lib && nmake install
+	BUILD_COMMAND cd build/lib && nmake clean install
 	BUILD_IN_SOURCE 1
 	# Installing
 	INSTALL_COMMAND ""
