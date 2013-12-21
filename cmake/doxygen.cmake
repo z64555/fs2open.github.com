a21d17596e9b6606e79b45acab4a50b002e10716
asarium <asarium@gmail.com>
2013-12-21 15:49:17 +0100

    Move the Doxygen output somehwere else

--- cmake/doxygen.cmake
+++ cmake/doxygen.cmake
@@ -1,10 +1,29 @@
 
 # add a target to generate API documentation with Doxygen
 find_package(Doxygen)
-if(DOXYGEN_FOUND)
+if(DOXYGEN_FOUND)	
+	file(MAKE_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/documentation/doxygen")
+	SET(OUTPUT_DIR "${CMAKE_CURRENT_BINARY_DIR}/documentation/doxygen")
+	FILE(TO_NATIVE_PATH ${OUTPUT_DIR} OUTPUT_DIR)
+	SET(OUTPUT_DIR \"${OUTPUT_DIR}\")
+	
+	SET(INPUTS "${CMAKE_CURRENT_SOURCE_DIR}/code" "${CMAKE_CURRENT_SOURCE_DIR}/freespace2" "${CMAKE_CURRENT_SOURCE_DIR}/fred2" "${CMAKE_CURRENT_SOURCE_DIR}/wxfred2")
+	
+	SET(INPUT_DIRS)
+	FOREACH(DIR IN LISTS INPUTS)
+		FILE(TO_NATIVE_PATH ${DIR} native_dir)
+		
+		LIST(APPEND INPUT_DIRS \"${native_dir}\")
+	ENDFOREACH(DIR)
+	STRING(REPLACE ";" " " INPUT_DIRS "${INPUT_DIRS}")
+	
+	CONFIGURE_FILE(${CMAKE_CURRENT_SOURCE_DIR}/fs2open.Doxyfile.in
+					${CMAKE_CURRENT_BINARY_DIR}/fs2open.Doxyfile
+					@ONLY)
+	
 	add_custom_target(doxygen
-		${DOXYGEN_EXECUTABLE} ${CMAKE_CURRENT_SOURCE_DIR}/fs2open.Doxyfile
-		WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+		${DOXYGEN_EXECUTABLE} ${CMAKE_CURRENT_BINARY_DIR}/fs2open.Doxyfile
+		WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
 		COMMENT "Generating API documentation with Doxygen" VERBATIM
 	)
 	
