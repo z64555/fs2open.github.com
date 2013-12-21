90d235027ca23051dfa232e0fcc905ca941bfb8b
asarium <asarium@gmail.com>
2013-12-21 14:37:41 +0100

    Make sure an EP is only built when actually needed

--- lib/stlport.cmake
+++ lib/stlport.cmake
@@ -15,15 +15,18 @@ ELSE(MSVC_USE_RUNTIME_DLL)
 	SET(STL_RUNTIME_LIBRARY "--with-static-rtl")
 ENDIF(MSVC_USE_RUNTIME_DLL)
 
+EP_CHECK_FILE_EXISTS("src/stlport/build/Makefiles/nmake/config.mak" CONFIGURE_COMMAND stlport test_configure.cmake "configure ${args} ${STL_RUNTIME_LIBRARY}" "src/stlport")
+EP_CHECK_FILE_EXISTS("src/stlport/lib/stlport.5.2.lib" BUILD_COMMAND stlport test_compile.cmake "cd build/lib && nmake clean install" "src/stlport")
+
 ExternalProject_Add(stlport
 	PREFIX stlport
 	# Downloading
 	URL http://sourceforge.net/projects/stlport/files/STLport/STLport-5.2.1/STLport-5.2.1.tar.gz # Full path or URL of source
 	URL_MD5 b20ace9f4a487b2614dfbb205bee112c               # MD5 checksum of file at URL
 	# Configuring, we need to use cmd as configure calls exit
-	CONFIGURE_COMMAND cmd /C configure ${args} ${STL_RUNTIME_LIBRARY}
+	CONFIGURE_COMMAND ${CONFIGURE_COMMAND}
 	# Building
-	BUILD_COMMAND cd build/lib && nmake clean install
+	BUILD_COMMAND ${BUILD_COMMAND}
 	BUILD_IN_SOURCE 1
 	# Installing
 	INSTALL_COMMAND ""
@@ -36,5 +39,5 @@ set_target_properties(stlport
 	
 ExternalProject_Get_Property(stlport source_dir)
 
-SET(STLPORT_INCLUDE_DIRS "${source_dir}/stlport" CACHE INTERNAL)
-SET(STLPORT_INCLUDE_LIB_DIRS "${source_dir}/lib" CACHE INTERNAL)
+SET(STLPORT_INCLUDE_DIRS "${source_dir}/stlport" CACHE INTERNAL "")
+SET(STLPORT_INCLUDE_LIB_DIRS "${source_dir}/lib" CACHE INTERNAL "")
