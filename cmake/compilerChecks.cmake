f126b6569d132c59f01218637b63672fd1f1b418
asarium <asarium@gmail.com>
2014-01-01 19:36:58 +0100

    Added some compiler checks

---
+++ cmake/compilerChecks.cmake
@@ -0,0 +1,38 @@
+
+if(CROSS_COMPILING)
+	return()
+endif(CROSS_COMPILING)
+
+include(CheckCXXSourceCompiles)
+
+CHECK_CXX_SOURCE_COMPILES("#include <type_traits>
+int main()
+{
+	return std::is_trivial<int>::value ? 1 : 0;
+}
+" HAS_CXX11_IS_TRIVIAL)
+
+CHECK_CXX_SOURCE_COMPILES("#include <type_traits>
+int main()
+{
+	return std::is_trivially_copyable<int>::value ? 1 : 0;
+}
+" HAS_CXX11_IS_TRIVIALLY_COPYABLE)
+
+CHECK_CXX_SOURCE_COMPILES("
+int main()
+{
+	static_assert(true, \"dummy\");
+	return 0;
+}
+" HAS_CXX11_STATIC_ASSERT)
+
+CHECK_CXX_SOURCE_COMPILES("
+int main()
+{
+	auto a = 42;
+	return a;
+}
+" HAS_CXX11_AUTO)
+
+configure_file(${CODE_HEADERS}/compiler.h.in ${GENERATED_SOURCE_DIR}/code/compiler.h)
