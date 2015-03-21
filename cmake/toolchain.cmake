a3eae60c6d3d5ba50fab0da5a5f0d5daf16e8897
asarium <asarium@gmail.com>
2015-03-21 13:30:03 +0100

    Fix toolchain.cmake

--- cmake/toolchain.cmake
+++ cmake/toolchain.cmake
@@ -6,20 +6,18 @@ SET(COMPILER_LBRARIES "" CACHE INTERNAL "Compiler specific libraries")
 
 ADD_DEFINITIONS(-DNO_CD_CHECK)
 
-if(${CMAKE_CXX_COMPILER_ID} STREQUAL "GNU")
+if("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
 	include(toolchain-gcc)
-elseif(${CMAKE_CXX_COMPILER_ID} STREQUAL "MSVC")
+elseif("${CMAKE_CXX_COMPILER_ID}" STREQUAL "MSVC")
 	include(toolchain-msvc)
-elseif(${CMAKE_CXX_COMPILER_ID} STREQUAL "Clang")
+elseif("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
 	include(toolchain-clang)
-ELSE(${CMAKE_CXX_COMPILER_ID} STREQUAL "GNU")
+ELSE("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
 	MESSAGE(STATUS "No special handling for this compiler present, good luck!")
-	
+
 	IF (NOT FSO_INSTRUCTION_SET STREQUAL "")
 		MESSAGE(WARNING "This compiler does not support a special instruction set!")
-	
+
 		SET(FSO_INSTRUCTION_SET "")
 	ENDIF(NOT FSO_INSTRUCTION_SET STREQUAL "")
-ENDIF(${CMAKE_CXX_COMPILER_ID} STREQUAL "GNU")
-
-
+ENDIF("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
