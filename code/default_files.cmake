3ded1805dcad8f4506c069def9d3380913cb6c4d
asarium <asarium@gmail.com>
2015-03-25 14:17:35 +0100

    Improve default files system

--- code/default_files.cmake
+++ code/default_files.cmake
@@ -1,9 +1,8 @@
 
 SET(DEF_OUT_FILES)
 FOREACH(file ${file_root_def_files})
-	FILE(RELATIVE_PATH FILE_NAME ${CMAKE_CURRENT_SOURCE_DIR} ${file})
-	
-	SET(OUTPUT "${GENERATED_SOURCE_DIR}/code/${FILE_NAME}.h")
+	set(INPUT_NAME "${CMAKE_CURRENT_SOURCE_DIR}/${file}")
+	SET(OUTPUT "${GENERATED_SOURCE_DIR}/code/${file}.h")
 
 	# For some reason this is needed...
 	GET_FILENAME_COMPONENT(DIRECTORY_PATH ${OUTPUT} PATH)
@@ -14,14 +13,14 @@ FOREACH(file ${file_root_def_files})
 	STRING(REPLACE "-" "_" FILENAME ${FILENAME})
 	STRING(REPLACE "." "_" FILENAME ${FILENAME})
 	STRING(REPLACE "#" "_" FILENAME ${FILENAME})
-	
+
 	ADD_CUSTOM_COMMAND(
 		OUTPUT ${OUTPUT}
-		COMMAND embedfile -text "${file}" "${OUTPUT}" "Default_${FILENAME}"
-		DEPENDS ${file}
-		COMMENT "Generating string file for ${file}"
+		COMMAND embedfile -text "${INPUT_NAME}" "${OUTPUT}" "Default_${FILENAME}"
+		DEPENDS ${INPUT_NAME}
+		COMMENT "Generating string file for ${INPUT_NAME}"
 		)
-	
+
 	LIST(APPEND DEF_OUT_FILES ${OUTPUT})
 ENDFOREACH(file ${file_root_def_files})
 
--- code/source_groups.cmake
+++ code/source_groups.cmake
@@ -117,29 +117,29 @@ set (file_root_debugconsole
 )
 
 SET(file_root_def_files
-	${CMAKE_CURRENT_SOURCE_DIR}/def_files/ai_profiles.tbl
-	${CMAKE_CURRENT_SOURCE_DIR}/def_files/autopilot.tbl
-	${CMAKE_CURRENT_SOURCE_DIR}/def_files/blur-f.sdr
-	${CMAKE_CURRENT_SOURCE_DIR}/def_files/brightpass-f.sdr
-	${CMAKE_CURRENT_SOURCE_DIR}/def_files/controlconfigdefaults.tbl
-	${CMAKE_CURRENT_SOURCE_DIR}/def_files/fonts.tbl
-	${CMAKE_CURRENT_SOURCE_DIR}/def_files/fxaa-f.sdr
-	${CMAKE_CURRENT_SOURCE_DIR}/def_files/fxaa-v.sdr
-	${CMAKE_CURRENT_SOURCE_DIR}/def_files/fxaapre-f.sdr
-	${CMAKE_CURRENT_SOURCE_DIR}/def_files/game_settings.tbl
-	${CMAKE_CURRENT_SOURCE_DIR}/def_files/iff_defs.tbl
-	${CMAKE_CURRENT_SOURCE_DIR}/def_files/ls-f.sdr
-	${CMAKE_CURRENT_SOURCE_DIR}/def_files/main-f.sdr
-	${CMAKE_CURRENT_SOURCE_DIR}/def_files/main-v.sdr
-	${CMAKE_CURRENT_SOURCE_DIR}/def_files/objecttypes.tbl
-	${CMAKE_CURRENT_SOURCE_DIR}/def_files/particle-f.sdr
-	${CMAKE_CURRENT_SOURCE_DIR}/def_files/particle-v.sdr
-	${CMAKE_CURRENT_SOURCE_DIR}/def_files/post-f.sdr
-	${CMAKE_CURRENT_SOURCE_DIR}/def_files/post-v.sdr
-	${CMAKE_CURRENT_SOURCE_DIR}/def_files/post_processing.tbl
-	${CMAKE_CURRENT_SOURCE_DIR}/def_files/species_defs.tbl
-	${CMAKE_CURRENT_SOURCE_DIR}/def_files/video-f.sdr
-	${CMAKE_CURRENT_SOURCE_DIR}/def_files/video-v.sdr)
+	def_files/ai_profiles.tbl
+	def_files/autopilot.tbl
+	def_files/blur-f.sdr
+	def_files/brightpass-f.sdr
+	def_files/controlconfigdefaults.tbl
+	def_files/fonts.tbl
+	def_files/fxaa-f.sdr
+	def_files/fxaa-v.sdr
+	def_files/fxaapre-f.sdr
+	def_files/game_settings.tbl
+	def_files/iff_defs.tbl
+	def_files/ls-f.sdr
+	def_files/main-f.sdr
+	def_files/main-v.sdr
+	def_files/objecttypes.tbl
+	def_files/particle-f.sdr
+	def_files/particle-v.sdr
+	def_files/post-f.sdr
+	def_files/post-v.sdr
+	def_files/post_processing.tbl
+	def_files/species_defs.tbl
+	def_files/video-f.sdr
+	def_files/video-v.sdr)
 
 # ExceptionHandler files
 set (file_root_exceptionhandler
