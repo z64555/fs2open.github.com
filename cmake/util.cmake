3a75e9d44a2bf88d9b49d041f6a763a531b2dda1
asarium <asarium@gmail.com>
2015-05-01 12:48:05 +0200

    Fix CMakeLists for older CMake versions

--- cmake/util.cmake
+++ cmake/util.cmake
@@ -5,12 +5,12 @@ ENDFUNCTION(MAKE_CACHE_INTERNAL)
 
 FUNCTION(ADD_IMPORTED_LIB NAME INCLUDES LIBS LIB_TYPE)
 	ADD_LIBRARY(${NAME} ${LIB_TYPE} IMPORTED GLOBAL)
-	
+
 	LIST(GET LIBS 0 MAIN_LIB)
 	LIST(LENGTH LIBS LIBS_SIZE)
 
 	SET(INTERFACE_LIBS)
-	
+
 	if(${LIBS_SIZE} GREATER 1)
 		math(EXPR LIBS_SIZE "${LIBS_SIZE} - 1")
 		foreach(i RANGE 1 ${LIBS_SIZE})
@@ -35,30 +35,30 @@ MACRO(PKG_CONFIG_LIB_RESOLVE NAME OUTVAR)
 					NAMES ${lib}
 					HINTS ${${NAME}_LIBDIR} ${${NAME}_LIBRARY_DIRS}
 		)
-	
+
 		if (NOT ${${lib}_LIBRARY} MATCHES ".*NOTFOUND.*")
 			SET(${OUTVAR} ${${OUTVAR}} ${${lib}_LIBRARY})
-		endif (NOT ${${lib}_LIBRARY} MATCHES ".*NOTFOUND.*") 
+		endif (NOT ${${lib}_LIBRARY} MATCHES ".*NOTFOUND.*")
 	endforeach(lib)
 ENDMACRO(PKG_CONFIG_LIB_RESOLVE)
 
 # Copy from http://cmake.3232098.n2.nabble.com/RFC-cmake-analog-to-AC-SEARCH-LIBS-td7585423.html
-INCLUDE (CheckFunctionExists) 
-INCLUDE (CheckLibraryExists) 
+INCLUDE (CheckFunctionExists)
+INCLUDE (CheckLibraryExists)
 
-MACRO (CMAKE_SEARCH_LIBS v_func v_lib func) 
-    CHECK_FUNCTION_EXISTS (${func} ${v_func}) 
-    IF (NOT ${v_func}) 
-        FOREACH (lib ${ARGN}) 
-            CHECK_LIBRARY_EXISTS (${lib} ${func} "" "HAVE_${func}_IN_${lib}") 
-            IF ("HAVE_${func}_IN_${lib}") 
-                SET (${v_func} TRUE) 
-                SET (${v_lib} "${lib}" CACHE INTERNAL "Library providing ${func}") 
-                BREAK() 
-            ENDIF ("HAVE_${func}_IN_${lib}") 
-        ENDFOREACH (lib) 
-    ENDIF (NOT ${v_func}) 
-ENDMACRO (CMAKE_SEARCH_LIBS) 
+MACRO (CMAKE_SEARCH_LIBS v_func v_lib func)
+    CHECK_FUNCTION_EXISTS (${func} ${v_func})
+    IF (NOT ${v_func})
+        FOREACH (lib ${ARGN})
+            CHECK_LIBRARY_EXISTS (${lib} ${func} "" "HAVE_${func}_IN_${lib}")
+            IF ("HAVE_${func}_IN_${lib}")
+                SET (${v_func} TRUE)
+                SET (${v_lib} "${lib}" CACHE INTERNAL "Library providing ${func}")
+                BREAK()
+            ENDIF ("HAVE_${func}_IN_${lib}")
+        ENDFOREACH (lib)
+    ENDIF (NOT ${v_func})
+ENDMACRO (CMAKE_SEARCH_LIBS)
 # End copy
 
 FUNCTION(EP_CHECK_FILE_EXISTS FILE OUTVAR TARGET NAME COMMAND WORKING_DIR)
@@ -69,7 +69,7 @@ FUNCTION(EP_CHECK_FILE_EXISTS FILE OUTVAR TARGET NAME COMMAND WORKING_DIR)
 	ELSE(WIN32)
 		SET(COMMAND "${COMMAND_STR}")
 	ENDIF(WIN32)
-	
+
 	FILE(WRITE "${CMAKE_CURRENT_BINARY_DIR}/${TARGET}/${NAME}" "
 IF(EXISTS \"${CMAKE_CURRENT_BINARY_DIR}/${TARGET}/${FILE}\")
 	MESSAGE(\"'${CMAKE_CURRENT_BINARY_DIR}/${TARGET}/${FILE}' already exists, nothing to be done.\")
@@ -77,7 +77,7 @@ ELSE(EXISTS \"${CMAKE_CURRENT_BINARY_DIR}/${TARGET}/${FILE}\")
 	execute_process(COMMAND ${COMMAND} WORKING_DIRECTORY \"${CMAKE_CURRENT_BINARY_DIR}/${TARGET}/${WORKING_DIR}\")
 ENDIF(EXISTS \"${CMAKE_CURRENT_BINARY_DIR}/${TARGET}/${FILE}\")
 ")
-	
+
 	SET(${OUTVAR} "${CMAKE_COMMAND}" -P ${CMAKE_CURRENT_BINARY_DIR}/${TARGET}/${NAME} PARENT_SCOPE)
 ENDFUNCTION(EP_CHECK_FILE_EXISTS)
 
@@ -90,3 +90,9 @@ MACRO(COPY_FILES_TO_TARGET _target)
 		)
 	ENDFOREACH(file)
 ENDMACRO(COPY_FILES_TO_TARGET)
+
+macro(set_policy policy value)
+	if (POLICY ${policy})
+		cmake_policy(SET ${policy} ${value})
+	endif ()
+endmacro(set_policy)
