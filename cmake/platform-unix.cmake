b35676597029b20885087f30d094e8d19a66244b
asarium <asarium@gmail.com>
2015-06-11 21:47:04 +0200

    Unify Unix variant handling

--- cmake/platform-unix.cmake
+++ cmake/platform-unix.cmake
@@ -9,9 +9,3 @@ FIND_PACKAGE(OpenGL REQUIRED)
 
 target_include_directories(platform INTERFACE ${PLATFORM_INCLUDES} ${OPENGL_INCLUDE_DIR})
 target_link_libraries(platform INTERFACE ${PLATFORM_LBRARIES} ${OPENGL_LIBRARY})
-
-IF(${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
-	include(platform-darwin)
-ELSE(${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
-	SET(EXE_GUI_TYPE)
-ENDIF(${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
--- cmake/platforms.cmake
+++ cmake/platforms.cmake
@@ -5,14 +5,21 @@ IF(FSO_CMAKE_DEBUG)
 	INCLUDE(debug-vars)
 ENDIF(FSO_CMAKE_DEBUG)
 
+# Initialize global variables
+set(IS_APPLE FALSE)
+
 IF(WIN32)
 	INCLUDE(platform-win32)
 ELSEIF(UNIX)
 	INCLUDE(platform-unix)
+
 	IF(${CMAKE_SYSTEM_NAME} MATCHES "FreeBSD")
 		INCLUDE(platform-freebsd)
 	ELSEIF(${CMAKE_SYSTEM_NAME} MATCHES "SunOS")
 		INCLUDE(platform-solaris)
+	ELSEIF(${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
+		set(IS_APPLE TRUE)
+		include(platform-darwin)
 	ENDIF(${CMAKE_SYSTEM_NAME} MATCHES "FreeBSD")
 ELSE(WIN32)
 	MESSAGE(STATUS "This platform is not supported, good luck!")
