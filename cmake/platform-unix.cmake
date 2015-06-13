cba8907ac4cc5822bea24c8b3edc671959839475
asarium <asarium@gmail.com>
2015-06-13 09:25:32 +0200

    Add special OpenGL interface library

--- cmake/platform-unix.cmake
+++ cmake/platform-unix.cmake
@@ -4,8 +4,3 @@ INCLUDE(util)
 MESSAGE(STATUS "Configuring UNIX specific things and stuff...")
 
 target_compile_definitions(platform INTERFACE SCP_UNIX USE_OPENAL)
-
-FIND_PACKAGE(OpenGL REQUIRED)
-
-target_include_directories(platform INTERFACE ${PLATFORM_INCLUDES} ${OPENGL_INCLUDE_DIR})
-target_link_libraries(platform INTERFACE ${PLATFORM_LBRARIES} ${OPENGL_LIBRARY})
