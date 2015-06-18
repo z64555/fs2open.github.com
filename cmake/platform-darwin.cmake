1f6b4803ee26edafb028546b18a7321a3a0ca0ee
asarium <asarium@gmail.com>
2015-06-18 17:06:54 +0200

    Fix syntax error

--- cmake/platform-darwin.cmake
+++ cmake/platform-darwin.cmake
@@ -5,7 +5,7 @@ SET(EXE_GUI_TYPE MACOSX_BUNDLE)
 FIND_LIBRARY(COCOA_LIBRARY Cocoa)
 
 if (NOT COCOA_LIBRARY)
-    message(FATAL_ERROR "Couldn't find Cocoa!");
+    message(FATAL_ERROR "Couldn't find Cocoa!")
 endif()
 
 target_link_libraries(platform INTERFACE ${COCOA_LIBRARY})
