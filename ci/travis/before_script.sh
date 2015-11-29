289c2d3666226563facf975bb6f77fb8a3642457
asarium <asarium@gmail.com>
2015-11-29 16:40:09 +0100

    Disable a warning when running in Travis

--- ci/travis/before_script.sh
+++ ci/travis/before_script.sh
@@ -6,6 +6,7 @@ mkdir -p travis-build
 cd travis-build
 
 if [ "$TRAVIS_OS_NAME" = "linux" ]; then
+    export CXXFLAGS="-Wno-array-bounds"
     $HOME/cmake/bin/cmake -G "Ninja" -DCMAKE_BUILD_TYPE=$CONFIGURATION -DFSO_FATAL_WARNINGS=ON ..
 elif [ "$TRAVIS_OS_NAME" = "osx" ]; then
     $HOME/cmake/CMake.app/Contents/bin/cmake -G "Xcode" -DFSO_FATAL_WARNINGS=ON ..
