93f1999911d066cccfc936a228dc2218a67f251a
asarium <asarium@gmail.com>
2015-11-29 23:11:38 +0100

    Improve CI scripts

--- ci/travis/script.sh
+++ ci/travis/script.sh
@@ -10,5 +10,5 @@ elif [ "$TRAVIS_OS_NAME" = "osx" ]; then
     cat "freespace2/fixup_bundle.cmake"
     cat "freespace2/fixup_bundle-$CONFIGURATION.cmake"
 	
-    $HOME/cmake/CMake.app/Contents/bin/cmake --build . --config "$CONFIGURATION" | xcpretty -c
+    $HOME/cmake/CMake.app/Contents/bin/cmake --build . --config "$CONFIGURATION" | xcpretty -f `xcpretty-travis-formatter` && exit ${PIPESTATUS[0]}
 fi
