21b6de12ad03fc98fcc53ef90375927fcfe40b30
asarium <asarium@gmail.com>
2015-11-30 12:08:32 +0100

    Testing...

--- ci/travis/script.sh
+++ ci/travis/script.sh
@@ -10,5 +10,5 @@ elif [ "$TRAVIS_OS_NAME" = "osx" ]; then
     cat "freespace2/fixup_bundle.cmake"
     cat "freespace2/fixup_bundle-$CONFIGURATION.cmake"
 	
-    $HOME/cmake/CMake.app/Contents/bin/cmake --build . --config "$CONFIGURATION" | tee xcodebuild.log | xcpretty -f `xcpretty-travis-formatter` && cat xcodebuild.log && exit ${PIPESTATUS[0]}
+    $HOME/cmake/CMake.app/Contents/bin/cmake --build . --config "$CONFIGURATION" | tee xcodebuild.log | xcpretty -f `xcpretty-travis-formatter` && find . && cat xcodebuild.log && exit ${PIPESTATUS[0]}
 fi
