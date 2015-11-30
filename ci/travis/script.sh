ac7dbe8d6e46b57bc1195dff6cf502571c0c6d34
asarium <asarium@gmail.com>
2015-11-30 19:14:40 +0100

    Remove debug code

--- ci/travis/script.sh
+++ ci/travis/script.sh
@@ -7,5 +7,5 @@ cd travis-build
 if [ "$TRAVIS_OS_NAME" = "linux" ]; then
     ninja
 elif [ "$TRAVIS_OS_NAME" = "osx" ]; then
-    $HOME/cmake/CMake.app/Contents/bin/cmake --build . --config "$CONFIGURATION" | tee xcodebuild.log | xcpretty -f `xcpretty-travis-formatter` && find . && cat xcodebuild.log && exit ${PIPESTATUS[0]}
+    $HOME/cmake/CMake.app/Contents/bin/cmake --build . --config "$CONFIGURATION" | xcpretty -f `xcpretty-travis-formatter` && exit ${PIPESTATUS[0]}
 fi
