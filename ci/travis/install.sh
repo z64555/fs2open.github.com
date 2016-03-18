0804b558a7f4c2defd3ba88540406dbc0041456a
asarium <asarium@gmail.com>
2016-03-18 22:27:53 +0100

    Use newer CMake version on travis

--- ci/travis/install.sh
+++ ci/travis/install.sh
@@ -5,13 +5,13 @@ set -e
 FILENAME=
 if [ "$TRAVIS_OS_NAME" = "linux" ]; then
     sudo apt-get install -y ninja-build libsdl2-dev
-    FILENAME=cmake-3.3.1-Linux-x86_64
+    FILENAME=cmake-3.4.3-Linux-x86_64
 elif [ "$TRAVIS_OS_NAME" = "osx" ]; then
     gem install xcpretty xcpretty-travis-formatter
-    FILENAME=cmake-3.3.1-Darwin-universal
+    FILENAME=cmake-3.4.3-Darwin-x86_64
 fi
 
 mkdir -p $HOME/cmake/
 
-wget -O /tmp/cmake.tar.gz --no-check-certificate http://www.cmake.org/files/v3.3/$FILENAME.tar.gz
+wget -O /tmp/cmake.tar.gz --no-check-certificate http://www.cmake.org/files/v3.4/$FILENAME.tar.gz
 tar -xzf /tmp/cmake.tar.gz -C $HOME/cmake/ --strip-components=1
