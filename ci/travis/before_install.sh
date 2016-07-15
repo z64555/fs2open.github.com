dabd9b5071875f40b5f57a46f910435d9e4aba0f
asarium <asarium@gmail.com>
2016-07-15 16:53:28 +0200

    Improve some things...

--- ci/travis/before_install.sh
+++ ci/travis/before_install.sh
@@ -3,8 +3,7 @@
 set -e
 
 if [ "$TRAVIS_OS_NAME" = "linux" ]; then
-    sudo add-apt-repository --yes ppa:zoogie/sdl2-snapshots
-    sudo apt-get update -qq
+    :
 elif [ "$TRAVIS_OS_NAME" = "osx" ]; then
     # Nothing to do here
     :
