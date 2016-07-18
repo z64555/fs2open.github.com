1aaa7852ccf7bb725d30706e752189b151d1bd74
asarium <asarium@gmail.com>
2016-07-18 15:18:21 +0200

    More fixes

--- ci/appveyor/before_build.ps1
+++ ci/appveyor/before_build.ps1
@@ -1,9 +1,9 @@
 New-Item build -type directory
 Set-Location -Path build
 
-FRED_STATUS="TRUE"
+$FRED_STATUS="TRUE"
 if ($Env:VS_VERSION -eq "10") {
-    FRED_STATUS="FALSE"
+    $FRED_STATUS="FALSE"
 }
 
 cmake -DCMAKE_BUILD_TYPE="%CONFIGURATION%" -DFSO_BUILD_FRED2=$FRED_STATUS -DFSO_USE_SPEECH=False -G "Ninja" ..
