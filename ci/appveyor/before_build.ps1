398712034e7d1951c2733163d4eda0aa7da7cb23
asarium <asarium@gmail.com>
2016-07-18 22:06:06 +0200

    Tests

--- ci/appveyor/before_build.ps1
+++ ci/appveyor/before_build.ps1
@@ -1,9 +1,10 @@
 New-Item build -type directory
 Set-Location -Path build
 
-$FRED_STATUS="TRUE"
+$FRED_STATUS="True"
 if ($Env:VS_VERSION -eq "10") {
-    $FRED_STATUS="FALSE"
+    $FRED_STATUS="False"
 }
 
+"cmake -DFSO_BUILD_FRED2=$FRED_STATUS -DFSO_USE_SPEECH=False -DFSO_USE_VOICEREC=False -G '$Env:CMAKE_GENERATOR' .."
 cmake -DFSO_BUILD_FRED2=$FRED_STATUS -DFSO_USE_SPEECH=False -DFSO_USE_VOICEREC=False -G "$Env:CMAKE_GENERATOR" ..
