ac878dd8e50865de68d10f8dc180a833e6fe3037
asarium <asarium@gmail.com>
2016-07-19 18:53:43 +0200

    Begin release script

---
+++ ci/travis/release.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env sh
+
+set -ex
+
+if [ "$TRAVIS_OS_NAME" = "linux" ]; then
+    for config in $BUILD_CONFIGS
+    do
+        cd "build/$config"
+        ninja install
+    done
+
+    ls -al /tmp/release
+    (cd /tmp/release && tar -cvzf /tmp/builds/$PACKAGE_NAME-builds-Linux.tar.gz *)
+elif [ "$TRAVIS_OS_NAME" = "osx" ]; then
+    cd build
+    
+    for config in $BUILD_CONFIGS
+    do
+        $HOME/cmake/CMake.app/Contents/bin/cmake --build . --config "$config" --target install | tee build.log \
+            | xcpretty -f `xcpretty-travis-formatter`
+        XCODE_RET=${PIPESTATUS[0]}
+        if [ "$XCODE_RET" -ne "0" ]; then
+            pastebin -e 1d -f build.log
+            exit $XCODE_RET
+        fi
+    done
+
+    ls -al /tmp/release
+    
+    (cd /tmp/release && tar -cvzf /tmp/builds/$PACKAGE_NAME-builds-MacOSX.tar.gz *)
+fi
