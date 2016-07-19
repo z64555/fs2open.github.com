7ccfd6053ebb1c5a3691634fabb4faf7cebd5689
asarium <asarium@gmail.com>
2016-07-19 19:53:53 +0200

    Install package the usual way

--- ci/travis/before_install.sh
+++ ci/travis/before_install.sh
@@ -3,7 +3,8 @@
 set -e
 
 if [ "$TRAVIS_OS_NAME" = "linux" ]; then
-    :
+    sudo add-apt-repository --yes ppa:george-edison55/cmake-3.x
+    sudo apt-get update -qq
 elif [ "$TRAVIS_OS_NAME" = "osx" ]; then
     # Nothing to do here
     :
--- ci/travis/install.sh
+++ ci/travis/install.sh
@@ -4,13 +4,10 @@ set -e
 
 FILENAME=
 if [ "$TRAVIS_OS_NAME" = "linux" ]; then
-    FILENAME=cmake-3.4.3-Linux-x86_64
+    sudo apt-get -y install cmake
 elif [ "$TRAVIS_OS_NAME" = "osx" ]; then
     gem install xcpretty xcpretty-travis-formatter thefox-pastebin
-    FILENAME=cmake-3.4.3-Darwin-x86_64
+    brew install cmake
 fi
 
-mkdir -p $HOME/cmake/
-
-wget -O /tmp/cmake.tar.gz --no-check-certificate http://www.cmake.org/files/v3.4/$FILENAME.tar.gz
-tar -xzf /tmp/cmake.tar.gz -C $HOME/cmake/ --strip-components=1
+cmake --version
