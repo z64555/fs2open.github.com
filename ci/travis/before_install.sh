7494d589066ac751e9b87184c673701e4f4d5e31
asarium <asarium@gmail.com>
2016-07-19 21:22:57 +0200

    Remove PPA

--- ci/travis/before_install.sh
+++ ci/travis/before_install.sh
@@ -3,7 +3,6 @@
 set -ex
 
 if [ "$TRAVIS_OS_NAME" = "linux" ]; then
-    sudo add-apt-repository --yes ppa:george-edison55/cmake-3.x
     sudo apt-get update -qq
 elif [ "$TRAVIS_OS_NAME" = "osx" ]; then
     # Nothing to do here
