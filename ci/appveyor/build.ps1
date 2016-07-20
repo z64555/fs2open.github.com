38ddf5d5ced72eef35244d3f72d2b7f3ec4fb4fa
asarium <asarium@gmail.com>
2016-07-20 10:27:07 +0200

    Begin Appveyor script

--- ci/appveyor/build.ps1
+++ ci/appveyor/build.ps1
@@ -1,5 +1,9 @@
-cmake --build . --config "$Env:CONFIGURATION" -- /verbosity:minimal
+if ([System.Convert]::ToBoolean($env:DeployBuild) -And [System.Convert]::ToBoolean($env:DeployConfig)) {
+    
+} else {
+    cmake --build . --config "$Env:CONFIGURATION" -- /verbosity:minimal
 
-if (! ($?)) {
-    exit 1
+    if (! ($?)) {
+        exit 1
+    }
 }
