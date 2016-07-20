c9774d25b326a042e5a8ec82f91075f8e4ee8053
asarium <asarium@gmail.com>
2016-07-20 11:10:23 +0200

    Disable RDP

--- ci/appveyor/before_build.ps1
+++ ci/appveyor/before_build.ps1
@@ -5,7 +5,7 @@ $AdditionalFeatures="ON"
 if ($Env:VS_VERSION -eq "10") {
     $AdditionalFeatures="OFF"
 } else {
-    $blockRdp = $true; iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/appveyor/ci/master/scripts/enable-rdp.ps1'))
+    #$blockRdp = $true; iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/appveyor/ci/master/scripts/enable-rdp.ps1'))
 }
 
 cmake -DFSO_BUILD_FRED2="${AdditionalFeatures}" -DFSO_USE_SPEECH="${AdditionalFeatures}" -DFSO_USE_VOICEREC="${AdditionalFeatures}" -G "$Env:CMAKE_GENERATOR" ..
