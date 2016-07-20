60fef81d2db3d70d49bfd7e62530fc1ac770c8c0
asarium <asarium@gmail.com>
2016-07-20 10:46:13 +0200

    Fix RDP stuff

--- ci/appveyor/before_build.ps1
+++ ci/appveyor/before_build.ps1
@@ -4,6 +4,8 @@ Set-Location -Path build
 $AdditionalFeatures="ON"
 if ($Env:VS_VERSION -eq "10") {
     $AdditionalFeatures="OFF"
+} else {
+    $blockRdp = $true; iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/appveyor/ci/master/scripts/enable-rdp.ps1'))
 }
 
 cmake -DFSO_BUILD_FRED2="${AdditionalFeatures}" -DFSO_USE_SPEECH="${AdditionalFeatures}" -DFSO_USE_VOICEREC="${AdditionalFeatures}" -G "$Env:CMAKE_GENERATOR" ..
