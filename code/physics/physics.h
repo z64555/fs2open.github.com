9ff5941419c236f89368bf0a77c531f0581f1cd6
z64555 <z1281110@gmail.com>
2017-01-26 07:15:07 -0600

    Doxy-fy and Edit comments into a (more) readable state

--- code/physics/physics.h
+++ code/physics/physics.h
@@ -32,66 +32,70 @@
 #define PF_GLIDING			(1 << 15)
 #define PF_FORCE_GLIDE		(1 << 16)
 
-//information for physics sim for an object
+/**
+ * @brief information for physics sim for an object
+ */
 typedef struct physics_info {
-	uint		flags;			//misc physics flags
+	uint flags;     //!< misc physics flags
 
-	float		mass;				//the mass of this object
-	vec3d		center_of_mass;		// Goober5000 - this is never ever used by physics; currently physics assumes the center of an object is the center of mass
-	matrix	I_body_inv;		// inverse moment of inertia tensor (used to calculate rotational effects)
+	float mass;             //!< the mass of this object
+	vec3d center_of_mass;   //!< Goober5000 - this is never ever used by physics; currently physics assumes the center of an object is the center of mass
+	matrix I_body_inv;      //!< inverse moment of inertia tensor (used to calculate rotational effects)
 
-	float		rotdamp;			//rotational velocity damping
-	float		side_slip_time_const;	// time const for achieving desired velocity in the local sideways direction
-												//   value should be zero for no sideslip and increase depending on desired slip
+	float rotdamp;              //!< rotational velocity damping
+	float side_slip_time_const; //!< time const for achieving desired velocity in the local sideways direction
+	                            //!<   value should be zero for no sideslip and increase depending on desired slip
 
-	float		delta_bank_const;	//const that heading is multiplied by. 0 means no delta bank.
+	float delta_bank_const; //!< const that heading is multiplied by. 0 means no delta bank.
 
-	vec3d	max_vel;			//maximum foward velocity in x,y,z
-	vec3d	afterburner_max_vel;	// maximum foward velocity in x,y,z while afterburner engaged
+	vec3d max_vel;              //!< maximum foward velocity in x,y,z
+	vec3d afterburner_max_vel;  //!< maximum foward velocity in x,y,z while afterburner engaged
 	vec3d booster_max_vel;
-	vec3d	max_rotvel;		//maximum p,b,h rotational velocity
-	float		max_rear_vel;	//maximum velocity in the backwards Z direction
+	vec3d max_rotvel;           //!< maximum p,b,h rotational velocity
+	float max_rear_vel;         //!< maximum velocity in the backwards Z direction
 
 	// Acceleration rates.  Only used if flag PF_ACCELERATES is set
-	// starting from rest	time to reach .50  v_max	0.69 time const
-	//								time to reach .75  v_max	1.39 time const
-	//
-	float		forward_accel_time_const;	// forward acceleration time const
-	float		afterburner_forward_accel_time_const;	// forward acceleration time const while afterburner engaged
-	float		booster_forward_accel_time_const;
-	float		forward_decel_time_const;	// forward deceleration time const
-	float		slide_accel_time_const;		// slide acceleration time const
-	float		slide_decel_time_const;		// slide deceleration time const
-	float		shockwave_shake_amp;			// amplitude of shockwave shake at onset
+	// starting from rest   time to reach .50  v_max    0.69 time const
+	//                      time to reach .75  v_max    1.39 time const
+
+	float forward_accel_time_const;             //!< forward acceleration time const
+	float afterburner_forward_accel_time_const; //!< forward acceleration time const while afterburner engaged
+	float booster_forward_accel_time_const;     //!< TODO: currently unused by anything
+	float forward_decel_time_const;     //!< forward deceleration time const
+	float slide_accel_time_const;       //!< slide acceleration time const
+	float slide_decel_time_const;       //!< slide deceleration time const
+	float shockwave_shake_amp;          //!< amplitude of shockwave shake at onset
 
 	// These get changed by the control code.  The physics uses these
 	// as input values when doing physics.
-	vec3d	prev_ramp_vel;				// follows the user's desired velocity
-	vec3d	desired_vel;				// in world coord
-	vec3d	desired_rotvel;			// in local coord
-	float		forward_thrust;			// How much the forward thruster is applied.  0-1.
-	float		side_thrust;			// How much the forward thruster is +x.  0-1.
-	float		vert_thrust;			// How much the forward thruster is +y.  0-1.
-		
-	// Data that changes each frame.  Physics fills these in each frame.
-	vec3d	vel;						// The current velocity vector of this object
-	vec3d	rotvel;					// The current rotational velecity (angles)
-	float		speed;					// Yes, this can be derived from velocity, but that's expensive!
-	float		fspeed;					//	Speed in the forward direction.
-	float		heading;
-	vec3d	prev_fvec;				//	Used in AI for momentum.
-	matrix	last_rotmat;			//	Used for moving two objects together and for editor.
+	vec3d prev_ramp_vel;    //!< follows the user's desired velocity
+	vec3d desired_vel;      //!< in world coord
+	vec3d desired_rotvel;   //!< in local coord
+	float forward_thrust;   //!< How much the forward thruster is applied.  0-1.
+	float side_thrust;      //!< How much the forward thruster is +x.  0-1.
+	float vert_thrust;      //!< How much the forward thruster is +y.  0-1.
 
-	int		afterburner_decay;	// timestamp used to control how long ship shakes after afterburner released
-	int		shockwave_decay;		// timestamp used to control how long ship affected after hit by shockwave
-	int		reduced_damp_decay;	// timestamp used to control how long ship ship has reduced damp physics	
+	// Data that changes each frame.  Physics fills these in each frame.
+	vec3d vel;              //!< The current velocity vector of this object
+	vec3d rotvel;           //!< The current rotational velecity (angles)
+	float speed;            //!< Yes, this can be derived from velocity, but that's expensive!
+	float fspeed;           //!< Speed in the forward direction.
+	float heading;
+	vec3d prev_fvec;        //!< Used in AI for momentum.
+	matrix last_rotmat;     //!< Used for moving two objects together and for editor.
+
+	int afterburner_decay;  //!< timestamp used to control how long ship shakes after afterburner released
+	int shockwave_decay;    //!< timestamp used to control how long ship affected after hit by shockwave
+	int reduced_damp_decay; //!< timestamp used to control how long ship ship has reduced damp physics	
 	
-	float	glide_cap;	//Backslash - for 'newtonian'-style gliding, the cap on velocity (so that something can't accelerate to ridiculous speeds... unless allowed to)
-	float	cur_glide_cap;	//SUSHI: Used for dynamic glide cap, so we can use the ramping function on the glide cap
-	float	glide_accel_mult;	//SUSHI: The acceleration multiplier for glide mode. A value < 0 means use glide ramping instead
-	bool use_newtonian_damp;	//SUSHI: Whether or not to use newtonian dampening
-	float afterburner_max_reverse_vel; //SparK: This is the reverse afterburners top speed vector
-	float afterburner_reverse_accel; //SparK: Afterburner's acceleration on reverse mode
+	float glide_cap;        //!< Backslash - for 'newtonian'-style gliding, the cap on velocity (so that something can't accelerate to ridiculous speeds... unless allowed to)
+	float cur_glide_cap;    //!< SUSHI: Used for dynamic glide cap, so we can use the ramping function on the glide cap
+	float glide_accel_mult; //!< SUSHI: The acceleration multiplier for glide mode. A value < 0 means use glide ramping instead
+	
+	bool  use_newtonian_damp;   //!< SUSHI: Whether or not to use newtonian dampening
+	
+	float afterburner_max_reverse_vel; //!< SparK: This is the reverse afterburners top speed vector
+	float afterburner_reverse_accel;   //!< SparK: Afterburner's acceleration on reverse mode
 } physics_info;
 
 // All of these are numbers from -1.0 to 1.0 indicating
